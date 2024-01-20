9739d982367076e3600764a37e93294ef7c258aa
tarunadityaselva <tarunadityaselva@gmail.com>
2024-01-20 20:10:25 +1100

    string constants

--- constants/constants.py
+++ constants/constants.py
@@ -14,7 +14,13 @@ wei_in_ether = 1000000000000000000
 max_uint256 = "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
 
 #minimum value of uint256
-MinInt256 = -0x8000000000000000000000000000000000000000000000000000000000000000
+min_int256 = -0x8000000000000000000000000000000000000000000000000000000000000000
 
 #maximum value of uint256
-MaxInt256 = 0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+max_int256 = 0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+
+#constant ether symbol "Ξ"
+ether_symbol = "\u039E"
+
+#constant eip-191 prefix
+eip191_prefix = "\x19Ethereum Signed Message:\n32"
