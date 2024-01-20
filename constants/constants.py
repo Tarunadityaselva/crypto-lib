806a775d98bb2c68274b21f04e9e98daefc73955
tarunadityaselva <tarunadityaselva@gmail.com>
2024-01-20 20:06:59 +1100

    constants for ethereum

---
+++ constants/constants.py
@@ -0,0 +1,20 @@
+#ethereum specific constant libs for hookable library and ease of use
+
+zero_address = "0x0000000000000000000000000000000000000000"
+
+zero_hash = "0x0000000000000000000000000000000000000000000000000000000000000000"
+
+#secp256k1 curve order(i.e: maximum number of points on the curve)
+secp256k1_order = "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141"
+
+#no of wei in 1 ether
+wei_in_ether = 1000000000000000000
+
+#maximum value of uint256
+max_uint256 = "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
+
+#minimum value of uint256
+MinInt256 = -0x8000000000000000000000000000000000000000000000000000000000000000
+
+#maximum value of uint256
+MaxInt256 = 0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
